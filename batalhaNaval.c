eb4dd77efa6acd0df612471d21b176412653a5bc
gabrielvz10 <gabrielvzavatto10@gmail.com>
2025-03-17 07:04:38 +0000

    atualizado para nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -12,18 +12,29 @@ int main() {
         }
     }
 
-    // Coordenadas iniciais dos navios
-    int linha_horizontal = 2, coluna_horizontal = 3; // Posição do navio horizontal
-    int linha_vertical = 5, coluna_vertical = 7;     // Posição do navio vertical
-
-    // Posiciona o navio horizontalmente (tamanho 3)
+    // Posicionando 4 navios:
+    // 1. Navio horizontal (linha 2, começando na coluna 1)
+    int linha_horizontal = 2, coluna_horizontal = 1;
     for (int i = 0; i < navio; i++) {
         tabuleiro[linha_horizontal][coluna_horizontal + i] = 3;
     }
 
-    // Posiciona o navio verticalmente (tamanho 3)
+    // 2. Navio vertical (coluna 6, começando na linha 5)
+    int linha_vertical = 5, coluna_vertical = 6;
     for (int i = 0; i < navio; i++) {
         tabuleiro[linha_vertical + i][coluna_vertical] = 3;
+    }
+
+    // 3. Navio diagonal ↘ (diagonal crescente, começando em 1,4)
+    int linha_diag1 = 1, coluna_diag1 = 4;
+    for (int i = 0; i < navio; i++) {
+        tabuleiro[linha_diag1 + i][coluna_diag1 + i] = 3;
+    }
+
+    // 4. Navio diagonal ↙ (diagonal decrescente, começando em 7,5)
+    int linha_diag2 = 7, coluna_diag2 = 5;
+    for (int i = 0; i < navio; i++) {
+        tabuleiro[linha_diag2 - i][coluna_diag2 + i] = 3;
     }
 
     // Exibe o tabuleiro no console
